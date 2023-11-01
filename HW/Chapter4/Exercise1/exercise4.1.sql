e32ff31f434ac2f775bd296fd4d32bb8bb1678de
cympyc1785 <cympyc1785@gmail.com>
2023-11-02 03:10:55 +0900

    Modify HW Indents

--- HW/Chapter4/Exercise1/exercise4.1.sql
+++ HW/Chapter4/Exercise1/exercise4.1.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `students` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(20) NOT NULL,
-	`admission_year` INT NOT NULL,
-	`major` VARCHAR(20) NOT NULL,
-	`individual_num` INT NOT NULL,
-	`phone_num` VARCHAR(20) NOT NULL,
-	`address` VARCHAR(100) NOT NULL,
-	`credit_sum` INT DEFAULT 0,
-	`average_credit` FLOAT DEFAULT 0.0,
-	`is_attending` TINYINT DEFAULT 0,
+	`id` 				INT NOT NULL AUTO_INCREMENT,
+	`name` 				VARCHAR(20) NOT NULL,
+	`admission_year`	INT NOT NULL,
+	`major` 			VARCHAR(20) NOT NULL,
+	`individual_num` 	INT NOT NULL,
+	`phone_num` 		VARCHAR(20) NOT NULL,
+	`address` 			VARCHAR(100) NOT NULL,
+	`credit_sum` 		INT DEFAULT 0,
+	`average_credit` 	FLOAT DEFAULT 0.0,
+	`is_attending` 		TINYINT DEFAULT 0,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
